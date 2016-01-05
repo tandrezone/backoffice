89b9c815593318a3facf7fcfc46fbd292fb391b2
Tiago André <tandre@moonlight.lan>
2016-01-05 16:07:31 +0000

    no message

---
+++ controllers/start.php
@@ -0,0 +1,7 @@
+<?php
+class start extends controller{
+  function index() {
+    return "This is the comand that runs when the route is /backoffice and you came to firstapp.dev or secondapp.dev";
+  }
+}
+ ?>
---
+++ routing.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * [$firstAppRouting Closure to internal routing, needed in all internal routes]
+ * To create a internal routing use $NAMERouting = function($router, $prefix){$router->addRoutes(array(array('GET','/'.$prefix, 'start.index')));}
+ */
+$intRoute = function($router,$prefix){
+  $router->addRoutes(array(
+
+    array('GET','/'.$prefix, 'start.index', 'moonlight/backoffice')
+  ));
+}
+
+
+
+ ?>
